295fcf09cdeaf691ef3fb8e87f6632bc8a709fbf
Martin O'Shea <martin.oshea@native-instruments.com>
2019-01-26 14:28:10 +0100

    Add start of GameStateManager

---
+++ src/tools/GameStateManager.cpp
@@ -0,0 +1,61 @@
+#include "GameStateManager.h"
+
+#include "../Data/TenantFactory.h"
+
+GameStateManager &GameStateManager::get()
+{
+    static GameStateManager gameStateManager;
+    return gameStateManager;
+}
+
+void GameStateManager::acceptTenant()
+{
+    m_acceptedTenants.push_back(m_currentTenant);
+    payNewTenantFee();
+    nextDay();
+}
+
+void GameStateManager::declineTenant()
+{
+    m_declinedTenants.push_back(m_currentTenant);
+    nextDay();
+}
+
+void GameStateManager::nextDay()
+{
+    m_days++;
+    generateNewTenant();
+}
+
+void GameStateManager::generateNewTenant()
+{
+    m_currentTenant = std::make_shared<TenantData>(TenantFactory::getTenant());
+    if (m_tenantChangedCallback)
+    {
+        m_tenantChangedCallback(m_currentTenant);
+    }
+}
+
+void GameStateManager::listenForTenantChanged(TenantChangedCallback function)
+{
+    m_tenantChangedCallback = function;
+}
+
+void GameStateManager::payNewTenantFee()
+{
+    changeCurrentMoney(newTenantFee);
+}
+
+void GameStateManager::changeCurrentMoney(int amount)
+{
+    m_currentMoneyEarned += amount;
+    if (amount > 0)
+    {
+        m_totalMoneyEarned += amount;
+    }
+}
+
+std::shared_ptr<TenantData> GameStateManager::getTenant()
+{
+    return m_currentTenant;
+}
