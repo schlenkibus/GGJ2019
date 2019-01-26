dcd23cd7f79c1c32643a30d34c12a218c1d74d29
schlenkibus <justus.scheil@parteimail.de>
2019-01-26 22:00:46 +0100

    added theoretical DayChangeWindow

---
+++ src/UI/GameStuff/DayChangeWindow.cpp
@@ -0,0 +1,8 @@
+#include "DayChangeWindow.h"
+#include "../../Game/GameStateManager.h"
+
+DayChangeWindow::DayChangeWindow() : GenericOkayWindowWithCallback{"Day " + std::to_string(GameStateManager::get().getDay()), "Okay", [](){
+    //Next Day GameStateManager::get().
+}} {
+
+}
